08920a5a0d2ec434c83effee80797405d4544a63
Marvin Hsin <bdfb1997@berkeley.edu>
2024-04-18 23:21:22 -0500

    initial commit

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import CreateRoomPage from './CreateRoomPage';
+import RoomJoinPage from './RoomJoinPage';
+import {BrowserRouter as Router, Routes, Route, Link, Redirect} from "react-router-dom";
+
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<p>This is the home page</p>} />
+                <Route path='/join' element={<RoomJoinPage />}/>
+                <Route path='/create' element={<CreateRoomPage />}/>
+            </Routes>
+        </Router>
+        );
+    }
+}
